010d9a7e24bad4bbcb5634538065b78af7ab59fc
Jarrett <moonwet@protonmail.com>
2020-07-16 14:51:19 -0500

    add updated lawyers script + csv

--- BusinessLawyers/spiders/Attorney_Spider.py
+++ BusinessLawyers/spiders/Attorney_Spider.py
@@ -1,21 +1,33 @@
 import scrapy
+import json
+import csv
 
 class LawyerSpider(scrapy.Spider):
 	name = 'lawyers'
+
+	custom_setting = {
+		'FEED_FORMAT':'csv',
+		'FEED_URI':'lawyers.csv'
+	}
 
 	base_url = 'https://www.justia.com/lawyers/business-law/texas/houston'
 
 	def start_requests(self):
 
-		yield scrapy.Request(
-			url = self.base_url,
-			callback = self.parse)
+		for page in range(0,25):
+			next_page = self.base_url + '?page=' + str(page)
+			yield scrapy.Request(url = next_page, callback = self.parse)
 
 
 	def parse(self, response):
+
 		for lawyer_card in response.css('div[class="lawyer-card-aligner"]'):
-			print(lawyer_card.css('strong[class="-phone"] a::text').get())
-			print(lawyer_card.css('a[class="url main-profile-link"] span::text').get())
+			items = {
+			'number': lawyer_card.css('strong[class="-phone"] a::text').get(),
+			'name': lawyer_card.css('a[class="url main-profile-link"] span::text').get()
+			}
+
+			yield items
 
 
 
